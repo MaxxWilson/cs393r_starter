37750de8ab70cfb27da042d01dab7a3bbc413416
Maxx Wilson <jessemaxxwilson@utexas.edu>
2021-10-01 01:10:43 -0500

    moves sensor frame tf, describe particle weighting

--- src/particle_filter/particle_filter.cc
+++ src/particle_filter/particle_filter.cc
@@ -82,18 +82,16 @@ void ParticleFilter::GetPredictedPointCloud(const Vector2f& loc,
   
   scan.resize((int)(num_ranges / resize_factor));
   
-  // Location of LIDAR in map frame
-  Vector2f laser_loc = loc + Vector2f(CONFIG_laser_offset*cos(angle), CONFIG_laser_offset*sin(angle));
   // Fill in the entries of scan using array writes, e.g. scan[i] = ...
   for (size_t i = 0; i < scan.size(); ++i) { // for each ray
     // Initialize the ray line
     line2f ray(0, 1, 2, 3);
     float ray_angle = angle + angle_min + resize_factor * i / num_ranges * (angle_max - angle_min);
-    ray.p0[0] = laser_loc[0] + range_min * cos(ray_angle);
-    ray.p0[1] = laser_loc[1] + range_min * sin(ray_angle);
-    ray.p1[0] = laser_loc[0] + range_max * cos(ray_angle);
-    ray.p1[1] = laser_loc[1] + range_max * sin(ray_angle);
-    Vector2f final_intersection = laser_loc + range_max * Vector2f(cos(ray_angle), sin(ray_angle));
+    ray.p0[0] = loc[0] + range_min * cos(ray_angle);
+    ray.p0[1] = loc[1] + range_min * sin(ray_angle);
+    ray.p1[0] = loc[0] + range_max * cos(ray_angle);
+    ray.p1[1] = loc[1] + range_max * sin(ray_angle);
+    Vector2f final_intersection = loc + range_max * Vector2f(cos(ray_angle), sin(ray_angle));
     double min_dist = range_max;
     
     for (size_t i = 0; i < map_.lines.size(); ++i) { // for each line in map
@@ -101,7 +99,7 @@ void ParticleFilter::GetPredictedPointCloud(const Vector2f& loc,
       Vector2f intersection_point; // Return variable
       bool intersects = map_line.Intersection(ray, &intersection_point);
       if (intersects) {
-        double cur_dist = (intersection_point - laser_loc).norm();
+        double cur_dist = (intersection_point - loc).norm();
         if(cur_dist < min_dist) {
           final_intersection = intersection_point;
           min_dist = cur_dist;
@@ -126,7 +124,8 @@ void ParticleFilter::Update(const vector<float>& ranges,
   // Get predicted point cloud
   Particle &particle = *p_ptr;
   vector<Vector2f> predicted_cloud;
-  GetPredictedPointCloud(particle.loc, 
+  Vector2f sensor_loc = BaseLinkToSensorFrame(particle.loc, particle.angle);
+  GetPredictedPointCloud(sensor_loc, 
                          particle.angle, 
                          ranges.size(), 
                          range_min, 
@@ -142,7 +141,12 @@ void ParticleFilter::Update(const vector<float>& ranges,
   float particle_weight = 0;
   // Calculate the particle weight
   for(int i = 0; i < predicted_cloud.size(); i++) {
-    float predicted_range = (predicted_cloud[i] - particle.loc).norm();
+    // Observation likelihood function, See Lecture 7, Slide 30
+    // 1) Center gaussian on predicted range
+    // 2) Evaluate gaussian at actual range to get likelihood
+    // 3) Multiply all scan likelihoods together for total particle weight
+    // *Note* Use log-likelihood to maintain precision
+    float predicted_range = (predicted_cloud[i] - sensor_loc).norm();
     float diff = abs(trimmed_ranges[i] - predicted_range);
     particle_weight += -diff; // smaller the diff, larger the particle weight
   }
@@ -215,6 +219,9 @@ void ParticleFilter::GetLocation(Eigen::Vector2f* loc_ptr,
   angle = 0;
 }
 
+Eigen::Vector2f BaseLinkToSensorFrame(const Vector2f &loc, const float &angle){
+  return loc + Vector2f(CONFIG_laser_offset*cos(angle), CONFIG_laser_offset*sin(angle));
+}
 
 }  // namespace particle_filter
 
