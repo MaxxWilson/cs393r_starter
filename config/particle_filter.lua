de23b0d27493bb2ceca9de832be57610dcce37e9
Maxx Wilson <jessemaxxwilson@utexas.edu>
2021-10-09 22:56:40 -0500

    new params

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -8,19 +8,19 @@ init_x_sigma = 0.0
 init_y_sigma = 0.0
 init_r_sigma = 0.00
 
-num_particles = 200 -- Prof recommends ~50
+num_particles = 100 -- Prof recommends ~50
 
 -- TODO Tune these from odometry messages and sampling time
-k1 = 0.5 -- translation error from translation
-k2 = 0.0 -- translation error from rotation
-k3 = 0.2 -- rotation error from translation
-k4 = 0.0 -- rotation error from rotation
+k1 = 0.05 -- translation error from translation
+k2 = 0.5 -- translation error from rotation
+k3 = 0.01 -- rotation error from translation
+k4 = 0.05 -- rotation error from rotation
 
 laser_offset = 0.2 --TODO measure real car offset
 resize_factor = 20 -- # num_points / resize_factor = num_rays
 
-sigma_observation = 0.2    -- Prof recommends 0.15-0.2 based on sensor specs
-gamma = 1/200               -- TODO Experimental tuning
+sigma_observation = 0.15    -- Prof recommends 0.15-0.2 based on sensor specs
+gamma = 1/50               -- TODO Experimental tuning
 
 dist_short = 0.1
 dist_long = 0.1
