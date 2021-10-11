9eb2b1c98bcd365449feb7223b982c2b347259c9
Maxx Wilson <JesseMaxxWilson@utexas.edu>
2021-10-10 20:24:06 -0500

    initial theory values

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -8,29 +8,28 @@ laser_offset = 0.2
 range_min = 0.02
 range_max = 10.0
 
--- Initial cloud distribution -- TODO tune from user error
-init_x_sigma = 0.0
-init_y_sigma = 0.0
-init_r_sigma = 0.00
+-- Initial cloud distribution --
+init_x_sigma = 0.33 -- 99% of the time, within a meter
+init_y_sigma = 0.33 -- 99% of the time, within a meter
+init_r_sigma = 0.1  -- 99% of the time, its within 17 deg
 
+-- Motion Model Params --
+k1 = 0.1  -- x error from translation          -- 95% of translations are within 15% margin of error
+k2 = 0.01   -- x error from rotation            -- This is effectively zero given the small angle approx
+k3 = 0.01   -- y error from translation         -- This is effectively zero given the small angle approx
+k4 = 0.02   -- y error from rotation            -- at 45 deg, 99% of values within 10 cm
+k5 = 0.1   -- rotation error from translation  -- at 1m, 99% of values within 7 deg
+k6 = 0.08  -- rotation error from rotation      -- 95% of translations are within 15% margin of error
+min_dist_to_update = 0.01                       -- Based on odometry messages at 1m/s at 40Hz
+
+-- Limited by computation --
 num_particles = 100 -- Increase until computation runs out
+resize_factor = 10          -- # num_points / resize_factor = num_rays
 
--- TODO Tune these from odometry messages and sampling time --
-k1 = 0.0 -- x error from translation
-k4 = 0.0 -- y error from rotation
-k5 = 0.0 -- rotation error from translation
-k6 = 0.0 -- rotation error from rotation
--- These go to zero for small angle approximation --
-k2 = 0.0 -- x error from rotation
-k3 = 0.0 -- y error from translation
+sigma_observation = 0.2    -- Prof recommends 0.15-0.2 based on sensor specs
+gamma = 1/50                -- TODO Experimental tuning
 
-resize_factor = 10 -- # num_points / resize_factor = num_rays
+dist_short = 0.2    -- 1 std from sensor 68.2%
+dist_long = 0.4     -- 2 std from sensor 95%
 
-sigma_observation = 0.15    -- Prof recommends 0.15-0.2 based on sensor specs
-gamma = 1/50               -- TODO Experimental tuning
-
-dist_short = 0.5
-dist_long = 0.5
-
-min_dist_to_update = 0.01    -- TODO Tuning from odom messages and sampling time
 resample_frequency = 40     -- TODO Experimental tuning
