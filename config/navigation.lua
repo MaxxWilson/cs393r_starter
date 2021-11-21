91f7b332312f9f27a366bc010b19034f92afa3f0
Maxx Wilson <JesseMaxxWilson@utexas.edu>
2021-11-21 15:17:33 -0600

    tuning avoidance gains

--- config/navigation.lua
+++ config/navigation.lua
@@ -10,10 +10,6 @@ range_max = 5.0
 map_length_dist = 50
 row_num = 2*(map_length_dist)/dist_res + 1
 
--- Pure Pursuit --
-pursuit_radius = 2.5 --meters
-goal_threshold = 0.5
-
 -- NEW PARAMS --
 length = 0.508 -- 20"
 width = 0.2667 -- 10.5"
@@ -38,11 +34,17 @@ safe_distance = 0.10 -- safe distance used in TOC control, stops with 5" left to
 max_path_length = 7.0
 curvature_increment = 0.02
 num_curves = (max_curvature - min_curvature)/curvature_increment
-
 clearance_factor = width/2 + safety_margin + 0.1 -- ~6"
-clearance_gain = 20.0
-dist_goal_gain = -1.5
 
 sys_latency = 1e9 * 0.215; -- ns
 sensing_latency =  1*sys_latency/4;
 actuation_latency = 3*sys_latency/4;
+
+----- Tunables -----
+-- Obstace Avoidance --
+clearance_gain = 10.0
+dist_goal_gain = -5.0
+
+-- Pure Pursuit --
+pursuit_radius = 2.5 -- meters
+goal_threshold = 0.5 -- meters
